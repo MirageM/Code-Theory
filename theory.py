95a7178ec4880e8620f4002ec5abcfd80563a7ca
MirageM <imiragesz@gmail.com>
2023-04-21 19:37:11 -0400

    strip removes leading and trailing whitespace char

--- theory.py
+++ theory.py
@@ -1,4 +1,22 @@
 '''
+In Python, strip() and split() are string methods that operate on a string and return a modified version of it
+'''
+'''
+strip() is used to remove leading and trailing whitespace characters (spaces, tabs, newlines) from a string.
+It returns a new string with the leadering and trailing whitespace removed.
+'''
+string_with_whitespace = "   hello    \n"
+stripped_string = string_with_whitespace.strip()
+print(stripped_string)
+
+
+
+
+
+
+
+
+'''
 Python, a list is mutable, which means that you can modify its contents by adding, remove, or changing elements.
 A tuple is immutable, which means that you cannot modify its contents
 Tuples make it useful for situations where the stored collection of items can't be changed, such as coodinates or database records. 
