fd8d31602d351efa791ebdc0070b7c2c94f96259
MirageM <imiragesz@gmail.com>
2023-04-20 23:56:03 -0400

    Python theory

---
+++ theory.py
@@ -0,0 +1,8 @@
+'''
+Python, a list is mutable, which means that you can modify its contents by adding, remove, or changing elements.
+A tuple is immutable, which means that you cannot modify its contents
+Tuples make it useful for situations where the stored collection of items can't be changed, such as coodinates or database records. 
+'''
+myTuple = (1, 2, 3, 4, 5)
+print(myTuple)
+print(myTuple[0])
