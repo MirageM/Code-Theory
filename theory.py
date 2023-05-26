68d20e0edd7191405635b818b9dc68e1599b4f49
MirageM <imiragesz@gmail.com>
2023-05-26 14:00:14 -0400

    Client client_socket.connect((server_host, server_port))

--- theory.py
+++ theory.py
@@ -1,3 +1,4 @@
+# SERVER:
 # Socket Programming with client and server
 import socket
 
@@ -30,6 +31,32 @@ client_socket.send(response.encode())
 # Close the connection
 client_socket.close()
 server_socket.close()
+
+# CLIENT:
+import socket
+
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the server host and port
+server_host = "localhost"
+server_port = 12345
+
+# Connect to the server
+client_socket.connect((server_host, server_port))
+print("Connected to server at {}:{}".format(server_host, server_port))
+
+# Send data to the server
+message = "Hello from the client!"
+client_socket.send(message.encode())
+
+# Receive a response from the server
+response = client_socket.recv(1024).decode()
+print("Received response:", response)
+
+# Close the connection
+client_socket.close()
+
 
 
 # Inheritance
