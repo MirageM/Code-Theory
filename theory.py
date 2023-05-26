404d5f45baa98d0ea82c963d3cbcc79dd3f092c4
MirageM <imiragesz@gmail.com>
2023-05-26 13:55:02 -0400

    import socket with client and server

--- theory.py
+++ theory.py
@@ -1,3 +1,37 @@
+# Socket Programming with client and server
+import socket
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define the host and port
+host = "localhost"
+port = 12345
+
+# Bind the socket to the host and port
+server_socket.bind((host, port))
+
+# Listen for incoming connections
+server_socket.listen(1)
+print("Server listening on {}:{}".format(host, port))
+
+# Accept a client connection
+client_socket, address = server_socket.accept()
+print("Connected to client:", address)
+
+# Receive data from the client
+data = client_socket.recv(1024).decode()
+print("Received data:", data)
+
+# Send a response to the client
+response = "Hello from the server"
+client_socket.send(response.encode())
+
+# Close the connection
+client_socket.close()
+server_socket.close()
+
+
 # Inheritance
 class Animal:
     def __init__ (self, name):
