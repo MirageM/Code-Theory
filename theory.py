14085954513205489f44e0fc51167faa072e6c6c
MirageM <imiragesz@gmail.com>
2023-05-27 12:32:23 -0400

    Server client connection

--- theory.py
+++ theory.py
@@ -1,3 +1,43 @@
+# Server
+import socket
+
+# Define the host and port
+host = 'localhost'
+port = 12345
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the host and port
+server_socket.bind((host, port))
+
+# Listen for incoming connections
+server_socket.listen(1)
+print('Server listening on {}:{}'.format(format, port))
+
+# Accept a client connection
+client_socket, address = server_socket.accept()
+print('Coneected to client:', address)
+
+while True:
+    # Receive data from the client
+    data = client_socket.recv(1024).decode()
+    if not data:
+        break
+    
+    print('Received data:', data)
+
+    # Process the data (here we simply echo it back)
+    response = 'Server received: ' + data
+
+    # Send the response back to the client
+    client_socket.send(response.encode())
+
+# Close the connection
+client_socket.close()
+server_socket.close()
+
+
 # Threading
 import threading
 import time
