25cf2f3f4d5197ab52f8241e8dae943a01a2eec5
MirageM <imiragesz@gmail.com>
2023-06-03 15:22:19 -0400

    two_sum(nums, target) -> List[int]

--- theory.py
+++ theory.py
@@ -11,7 +11,7 @@ from typing import List
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     complement_map = {}
-    for i, num in enumerate(nums);
+    for i, num in enumerate(nums):
         complement = target - num
         if complement in complement_map:
             return [complement_map[complement], i]
