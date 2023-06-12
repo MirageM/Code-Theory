fbf5f20ad8d965ce5c011ae970a4474684dd8367
MirageM <imiragesz@gmail.com>
2023-06-11 23:29:45 -0400

    is_palindrome text == text[::-1]

--- theory.py
+++ theory.py
@@ -1,6 +1,12 @@
 def reverse_string(text):
     return text[::-1]
 
+def is_palindrome(text):
+    return reverse_string(text) == text
+
+def is_palindrome2(text):
+    return text == text[::-1]
+
 # Calculate Area and Perimeter
 import math
 
