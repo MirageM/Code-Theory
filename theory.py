2496cbea8c7d453d52b736fbfe13713b90757322
MirageM <imiragesz@gmail.com>
2023-06-13 15:55:03 -0400

    file.write(content)

--- theory.py
+++ theory.py
@@ -2,6 +2,10 @@ def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
 from typing import List
 
 def is_prime(number):
