556d0eba3d10b10c0af6e9eafa10c91411f05b86
MirageM <imiragesz@gmail.com>
2023-06-14 16:29:16 -0400

    insertion_sort j+ 1 = key

--- theory.py
+++ theory.py
@@ -8,6 +8,16 @@ def bubble_sort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
     return array
 
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i -1
+        while j >= 0 and array[j] > key:
+            array[j +1] = array[j]
+            j -= 1
+        array[j+1] = key
+    return array
+
 def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read()
