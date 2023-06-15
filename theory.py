151699fcc0afb66cdcd896c60911865ce8ee4470
MirageM <imiragesz@gmail.com>
2023-06-15 14:23:37 -0400

    generate_fibonacci(n) next_number = sequence[-1] + sequence[-2]

--- theory.py
+++ theory.py
@@ -1,5 +1,14 @@
 from typing import List
 
+def generate_fibonacci(n):
+    sequence = [0, 1]
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+    return sequence
+
+from typing import List
+
 def bubble_sort(array):
     n = len(array)
     for i in range(n):
