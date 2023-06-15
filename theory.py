3b7123343261040145c442d08f65e4d541bae380
MirageM <imiragesz@gmail.com>
2023-06-15 14:29:30 -0400

    get_nth_fibonacci(n) a, b = b, a + b

--- theory.py
+++ theory.py
@@ -15,6 +15,16 @@ def is_fibonacci(number):
         a, b = b, a + b
     return b == number
 
+def get_nth_fibonacci(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
 from typing import List
 
 def bubble_sort(array):
