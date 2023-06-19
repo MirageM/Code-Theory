85983bc54e832b5268e22b4bc2211b351e16380a
MirageM <imiragesz@gmail.com>
2023-06-19 13:40:48 -0400

    is_prime, generate_primes, print_primes

--- theory.py
+++ theory.py
@@ -1,3 +1,25 @@
+from typing import List
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_primes(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+def print_primes(primes):
+    for prime in primes:
+        print(prime)
+
+
 import threading
 
 # Function to be executed in a separate thread
